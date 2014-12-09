a1f966e71c4090beaadd40d508bf2121e830664e
administrator <administrator@static-182.74.99.14-tataidc.co.in>
2014-12-09 08:31:32 +0530

    Seed project for Angular, Created By Yoman

---
+++ AngularJS/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularJsApp
+ * @description
+ * # angularJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularJsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
