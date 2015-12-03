a1a3f4010b8ee1682b1d07d7836f0d1dca811b13
Gunjan <kumargun@yahoo.com>
2015-12-03 17:43:20 +0530

    Add Job UI Work

--- toilPortal/toilSrvProvider/db.js
+++ toilPortal/toilSrvProvider/db.js
@@ -2,9 +2,16 @@
  * Created by gunjan.kumar on 28/11/15.
  */
 var mysql      = require('mysql');
+/*var pool = mysql.createPool({
+    connectionLimit : 100,
+    host     : 'toiltalent.c3hfg7tttfy1.eu-west-1.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'toilTalent',
+    database    : 'toil'
+});*/
 var pool = mysql.createPool({
     connectionLimit : 100,
-    host     : '127.0.0.1',
+    host     : 'localhost',
     user     : 'root',
     password : 'gunjan',
     database    : 'toil'
